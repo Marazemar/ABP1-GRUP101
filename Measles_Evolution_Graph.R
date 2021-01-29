14098428b1a2dd973465540a0ccf58dee8e0eed8
Marazemar <72250088+Marazemar@users.noreply.github.com>
2021-01-29 17:00:14 +0100

    Add files via upload

---
+++ Measles_Evolution_Graph.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+info <- read.csv("reported-cases-of-measles.csv", sep=",", stringsAsFactors = TRUE)
+years <- factor(info$Year)
+infoany <- split(info$Reported.cases.of.measles..WHO..2019.., years)
+sumaany <- sapply(infoany, sum, na.rm=TRUE)
+final <- data.frame(ANY=names(sumaany), INFECTATS=c(sumaany))
+plot(final, lwd=2 , type="l" , bty="n" , main= "Evolució del Xarampió al món", ylab="INFECTATS" , col=rgb(0.1,0.7,0.6,0.7))
